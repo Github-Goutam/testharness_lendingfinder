e2aaccb6c0ed5500358251bc1e69d2a046ef2d43
JananiR23 <61050498+JananiR23@users.noreply.github.com>
2020-07-01 12:30:01 +0530

    updated

--- src/main/java/com/rbs/lendingfinder/model/LendingFinderAttributeRequest.java
+++ src/main/java/com/rbs/lendingfinder/model/LendingFinderAttributeRequest.java
@@ -10,57 +10,74 @@ public class LendingFinderAttributeRequest {
 	private Integer applicationIdentity;
 	private String userId;
 	private Integer purpose;
-	private Boolean currentAcctFlag;
-	private Boolean overdraftFlag;
+	private List<Integer> currentAcctFlag;
+	private List<Integer> overdraftFlag;
 	
-	
-	public List<Integer> getBorrowingAmount() {
-		return borrowingAmount;
-	}
-	public void setBorrowingAmount(List<Integer> borrowingAmount) {
-		this.borrowingAmount = borrowingAmount;
-	}
 	public List<Integer> getTermFactor() {
 		return termFactor;
 	}
+
 	public void setTermFactor(List<Integer> termFactor) {
 		this.termFactor = termFactor;
 	}
+
+
 	public Integer getBankDivision() {
 		return bankDivision;
 	}
+
 	public void setBankDivision(Integer bankDivision) {
 		this.bankDivision = bankDivision;
 	}
+
+	
 	public Integer getApplicationIdentity() {
 		return applicationIdentity;
 	}
+
 	public void setApplicationIdentity(Integer applicationIdentity) {
 		this.applicationIdentity = applicationIdentity;
 	}
+
+	
+
+	public List<Integer> getBorrowingAmount() {
+		return borrowingAmount;
+	}
+
+	public void setBorrowingAmount(List<Integer> borrowingAmount) {
+		this.borrowingAmount = borrowingAmount;
+	}
+
 	public String getUserId() {
 		return userId;
 	}
+
 	public void setUserId(String userId) {
 		this.userId = userId;
 	}
 	public Integer getPurpose() {
 		return purpose;
 	}
+
 	public void setPurpose(Integer purpose) {
 		this.purpose = purpose;
 	}
-	public Boolean getCurrentAcctFlag() {
+	public List<Integer> getCurrentAcctFlag() {
 		return currentAcctFlag;
 	}
-	public void setCurrentAcctFlag(Boolean currentAcctFlag) {
+
+	public void setCurrentAcctFlag(List<Integer> currentAcctFlag) {
 		this.currentAcctFlag = currentAcctFlag;
 	}
-	public Boolean getOverdraftFlag() {
+
+	public List<Integer> getOverdraftFlag() {
 		return overdraftFlag;
 	}
-	public void setOverdraftFlag(Boolean overdraftFlag) {
+
+	public void setOverdraftFlag(List<Integer> overdraftFlag) {
 		this.overdraftFlag = overdraftFlag;
-	}	
-	
+	}
+
+
 }
